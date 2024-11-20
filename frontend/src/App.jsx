e843373e87fa0480fa61bab0b86ed6fb60348bad
marpuriganesh <marpuriganesh@gmail.com>
2024-11-20 17:50:59 +0530

    feat(PageNotFound):

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import Counter from "./components/counter";
 import User from "./components/User";
+import PageNotFound from "./components/PageNotFound";
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { useState } from "react";
@@ -8,19 +10,19 @@ import { useState } from "react";
 import { MyContext } from "./components/Context";
 
 function App() {
-  
-  const [user,setUser] = useState("")
+  const [user, setUser] = useState("");
 
   return (
     <>
       <div className="App">
-        <MyContext.Provider value={{user,setUser}}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/counter" element={<Counter user={user} />} />
-            <Route path="/" element={<User/>}/>
-          </Routes>
-        </BrowserRouter>
+        <MyContext.Provider value={{ user, setUser }}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/counter" element={<Counter user={user} />} />
+              <Route path="/" element={<User />} />
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </BrowserRouter>
         </MyContext.Provider>
       </div>
     </>
