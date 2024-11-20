ec0446a9f83d6c373e22fef99971e6eb2a694b8c
marpuriganesh <marpuriganesh@gmail.com>
2024-11-21 00:49:05 +0530

    feat(.env):

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,28 @@
-const express = require("express")
-const mysql = require("mysql2/promise")
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const mysql = require("mysql2/promise");
+const app = express();
+// console.log(process.env.DB_HOST,process.env.DB_USER,process.env.DB_PASSWORD,process.env.DB_DATABASE)
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'ganesh',
-    password: 'Ganesh@4263',
-    database: 'react_team_tasks'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
+app.get("/", async (req, res) => {
+  console.log("new request came!");
+  // const sql = 'INSERT INTO stored_values (username, value) VALUES (?, ?)';
+  // const values = ["test","4"]
+  try {
+    // const [result] = await pool.query(sql, values);
+    // res.json({ message: 'User created successfully', id: result.insertId });
+    const [rows] = await pool.query("SELECT * FROM stored_values");
+    res.json(rows);
+  } catch (error) {
+    console.error();
+  }
+});
 
-app.get("/",async (req,res)=>{
-    console.log("new request came!")
-    const sql = 'INSERT INTO stored_values (username, value) VALUES (?, ?)';
-    const values = ["test","4"]
-    try{
-        const [result] = await pool.query(sql, values);
-        // res.json({ message: 'User created successfully', id: result.insertId });
-        const [rows] = await pool.query('SELECT * FROM stored_values');
-        res.json(rows);
-    }
-    catch(error){
-        console.error()
-    }
-})
-
-app.listen(3000)
+app.listen(3000);
